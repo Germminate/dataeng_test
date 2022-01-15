57aa1abe51a6506501b4fb21037966a458cab2d5
Germaine Tan <36045318+Germminate@users.noreply.github.com>
2022-01-15 20:35:13 +0800

    updated module structure and improved deployability (#9)

---
+++ data_pipeline/docker/build_and_run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Prevent continuation if one step fails
+set -euo pipefail
+
+# Get current drive
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+echo "Building docker image ..."
+sudo docker build --network=host \
+    -t data_pipeline \
+    -f $DIR/Dockerfile \
+    $DIR/..
+
+
+sudo docker run --privileged --rm -it --net=host --shm-size=1gb \
+-e DISPLAY \
+-e TERM=xterm-256color \
+$@ \
+data_pipeline:latest
